5b922b222ad680c4248df6d80625ddfb8134e541
Marcoshipp <marcobelesay@gmail.com>
2024-01-04 21:27:38 +0800

    refactored a bit of code

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -6,24 +6,25 @@ import org.junit.Test;
 import student.StudentArrayDeque;
 
 public class TestArrayDequeEC {
+    private final int N = 5000;
+    private final int M = 100;
     @Test
     public void randomizedTest() {
         StudentArrayDeque<Integer> studentList = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solutionList = new ArrayDequeSolution<>();
         StringBuilder errorMessage = new StringBuilder();
-        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int choice = StdRandom.uniform(0, 6);
             switch (choice) {
                 case 0: {
-                    int number = StdRandom.uniform(0, 100);
+                    int number = StdRandom.uniform(0, M);
                     studentList.addFirst(number);
                     solutionList.addFirst(number);
                     errorMessage.append("addFirst" + "(" + number + ")" + "\n");
                     break;
                 }
                 case 1: {
-                    int number = StdRandom.uniform(0, 100);
+                    int number = StdRandom.uniform(0, M);
                     studentList.addLast(number);
                     solutionList.addLast(number);
                     errorMessage.append("addLast" + "(" + number + ")" + "\n");
@@ -35,19 +36,25 @@ public class TestArrayDequeEC {
                     break;
                 }
                 case 3: {
-                    if (studentList.isEmpty() || solutionList.isEmpty()) continue;
+                    if (studentList.isEmpty() || solutionList.isEmpty()) {
+                        continue;
+                    }
                     errorMessage.append("removeFirst" + "(" + ")" + "\n");
                     assertEquals(errorMessage.toString(), solutionList.removeFirst(), studentList.removeFirst());
                     break;
                 }
                 case 4: {
-                    if (studentList.isEmpty() || solutionList.isEmpty()) continue;
+                    if (studentList.isEmpty() || solutionList.isEmpty()) {
+                        continue;
+                    }
                     errorMessage.append("removeLast" + "(" + ")" + "\n");
                     assertEquals(errorMessage.toString(), solutionList.removeLast(), studentList.removeLast());
                     break;
                 }
                 case 5: {
-                    if (studentList.isEmpty() || solutionList.isEmpty()) continue;
+                    if (studentList.isEmpty() || solutionList.isEmpty()) {
+                        continue;
+                    }
                     int number = StdRandom.uniform(0, solutionList.size());
                     errorMessage.append("get" + "(" + number + ")" + "\n");
                     assertEquals(errorMessage.toString(), solutionList.get(number), studentList.get(number));
